b39d26811132dcc90957c8bf96ac15a20c42f99a
sophie-kim <keun195@gmail.com>
2019-08-10 22:15:52 -0700

    clock js update

---
+++ clock.js
@@ -0,0 +1,16 @@
+//clock.js
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
+function init(){
+    setInterval(getTime, 100);
+}
+
+init();
