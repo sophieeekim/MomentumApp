6d8dfc9828d552d55ddc0ec523a5ffd3b46c9cb7
sophie-kim <keun195@gmail.com>
2019-08-13 14:37:38 -0700

    todo.js save function update

--- todo.js
+++ todo.js
@@ -5,16 +5,31 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
+const toDos = []; // array for to do list things 
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+    // Use JSON.stringify to change object to string. 
+    // In local storage, you can save only string! Not JavaScript data.
+}
 
 function paintToDo(text){
    const li = document.createElement("li");
    const delBtn = document.createElement("button");
-   delBtn.innerText = "❌";
    const span = document.createElement("span");
+   const newId = toDos.length + 1;
+   delBtn.innerText = "❌";
    span.innerText = text;
-   li.appendChild(span);
    li.appendChild(delBtn);
+   li.appendChild(span);
+   li.id = newId; 
    toDoList.appendChild(li);
+   const toDoObj = {
+       text: text,
+       id: newId
+   };
+   toDos.push(toDoObj);
+   saveToDos(); 
 }
 
 function handleSubmit(){
@@ -25,9 +40,12 @@ function handleSubmit(){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-   
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach( function(toDo) {
+            paintToDo(toDo.text); 
+        });
     } 
 }
 
