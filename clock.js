f33c5a96d8bfff575cde63fd1f7377bbac2374a5
sophie-kim <keun195@gmail.com>
2019-08-15 23:19:22 -0700

    minor css change

--- clock.js
+++ clock.js
@@ -1,6 +1,20 @@
 //clock.js
 const clockContainer = document.querySelector(".js-clock"); //return first element that matches with the query selector
 const clockTitle = clockContainer.querySelector("h1");
+let hourRange = 0; 
+// 0 morning, 1 afternoon, 2 evening, 3 night
+
+function setHourRange(hours){
+    if(hours < 5 || hours > 22){
+        hourRange = 3; //night
+    } else if(hours >=5 && hours < 12){
+        hourRange = 0;
+    } else if(hours >=12 && hours <17){
+        hourRange = 1;
+    } else {
+        hourRange = 2;
+    }
+}
 
 function getTime(){
     const date = new Date();
@@ -14,10 +28,12 @@ function getTime(){
     }:${
         seconds < 10 ? `0${seconds}`: seconds
     }`;
+    setHourRange(hours);
 }
 function init(){
     getTime();
     setInterval(getTime, 1000);
+   
 }
 
 init();
--- greeting.js
+++ greeting.js
@@ -1,8 +1,14 @@
 //greeting.js
-
+/*
+Morning (5AM - 12PM) "Good Morning,  " 
+Afternoon (12PM - 5PM) "Good Afternoon, let's grab a coffee"
+Evening (5PM - 10PM) "Good Evening, netflix time?"
+Night (10PM - 5AM) "Good Night, have a sweet dream! "
+*/
 const form = document.querySelector(".js-form");
 const input = form.querySelector("input");
 const greeting = document.querySelector(".js-greetings");
+let greetingText = "Hello";
 
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
@@ -20,10 +26,31 @@ function askForName(){
     form.classList.add(SHOWING_CN);
     form.addEventListener("submit", handleSubmit);
 }
+
+function selectGreeting(hourRange){
+    switch(hourRange){
+        case 0: //morning
+            greetingText = "It's time to work!";
+            break;
+        case 1: //afternoon
+            greetingText = "let's grab a coffee!";
+            break;
+        case 2: //evening
+            greetingText = "Netflix time?";
+            break;
+        case 3:
+            greetingText = "have a sweet dream! ";
+            break;
+        default:
+            break;
+    }
+}
+
 function paintGreeting(text){
+    selectGreeting(hourRange);
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `${text}, ${greetingText}`;
 }
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
